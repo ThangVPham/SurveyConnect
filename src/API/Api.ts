ef8bfbe83d0ab78c07a790a7cac79645dc28be2d
Thang Pham <thangpham1412@gmail.com>
2023-10-17 12:29:19 -0400

    added survey_post_api

--- src/API/Api.ts
+++ src/API/Api.ts
@@ -2,4 +2,4 @@ export const BASE_API = "https://surveyconnect-backend.onrender.com/api";
 export const LOGIN_API = "https://surveyconnect-backend.onrender.com/api/user/login";
 export const REGISTER_API = "https://surveyconnect-backend.onrender.com/api/user/register";
 export const SURVEY_API = "https://surveyconnect-backend.onrender.com/api/surveys";
-export const SURVEY_POST_API = "http://localhost:5000/api/surveys/";
+export const SURVEY_POST_API = "https://surveyconnect-backend.onrender.com/api/surveys";
